56dd91c460018d1c6142bb0d36d568a84a34e02f
Samuel Li <samuelgh.li@gmail.com>
2021-04-13 19:21:24 +1000

    rename interface

---
+++ client/src/about-me.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+  } from "react-router-dom";
+  function aboutme() {
+    return (
+      
+        <div>
+        <p>test</p>
+        </div>
+      
+    
+    )};
+  
+export default aboutme;
